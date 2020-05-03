588399dcfc8c57a9f72e295e982f4c791a999cc8
Manvi Madan <manvimadan@outlook.com>
2020-05-03 20:57:10 +1200

    removed duplicates

--- SuperLearner.R
+++ SuperLearner.R
@@ -7,4 +7,21 @@ bp <- read.csv(file = '/home/manvi/Documents/Life_long_learning/Projects/Predict
 
 dataset <- inner_join( bp, patient, by = c("patientunitstayid" = "patientunitstayid"))
 
+#checking if our dataset has duplicate records
+duplicated(dataset$patientunitstayid)
 
+#extracting duplicate elements
+dataset[duplicated(dataset$patientunitstayid), ]
+
+#removing duplicates from the dataset
+dataset <- dataset[!duplicated(dataset$patientunitstayid),] #count has become half od the individual tables as both patient and bp datasets have duplicates
+
+#checking the classifiers in the superlearner library
+library(SuperLearner)
+listWrappers()
+
+#splitting the dataset into test train and validayion dataset
+
+
+
+  
